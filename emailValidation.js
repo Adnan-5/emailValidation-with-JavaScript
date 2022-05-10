7564c919b08b91dac6a7237be6e3b6386ff59cd4
Adnan-5 <sahilladnan810@gmail.com>
2022-05-10 12:33:02 +0500

    regex updated to verify a valied email

--- emailValidation.js
+++ emailValidation.js
@@ -1,5 +1,5 @@
 function ValidateEmail(inputText) {
-    var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+    var mailformat = /[aA-zZ0-9+/,_-]+@[aA-zZ0-9_,.-]+\.[aA-zZ0-9.,-]+/g;
     if (inputText.value.match(mailformat)) {
         alert("You have entered a valid email address!"); // The pop up alert for a valid email address
         document.form1.text1.focus();
